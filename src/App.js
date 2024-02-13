4b4e8e8786b6427ce0490d1f30deaf8ffc0d2bde
yarin1890 <104309089+yarin1890@users.noreply.github.com>
2024-02-13 17:28:23 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <Container className="my-5">
             <h1 className="text-center mb-4">Cost Manager Web App</h1>
-            <h3 className="text-center mb-4">By Nissim Amira & Yarin Cohen</h3>
+            <h3 className="text-center mb-4">By Nissim Amira & Yarin Ben Moshe</h3>
             <AddCostForm />
             <CostReport />
         </Container>
